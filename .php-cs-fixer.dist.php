f1ee4e62a08d8e74b2b242f499960328e5bc8d32
Olivier Dolbeau <olivier@bbnt.me>
2022-10-23 12:35:51 +0200

    Improve php-cs-fixer config

--- .php-cs-fixer.dist.php
+++ .php-cs-fixer.dist.php
@@ -14,6 +14,9 @@ return (new PhpCsFixer\Config())
     ->setRules(array(
         '@Symfony' => true,
         '@Symfony:risky' => true,
+        'declare_strict_types' => true,
+        'no_useless_else' => true,
+        'no_useless_return' => true,
     ))
     ->setRiskyAllowed(true)
     ->setFinder($finder)
